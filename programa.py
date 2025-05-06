80dde2d2e104c556d931854e3aca4a5cc07cbb0a
Luis Salvatore <luiseduardosalvatore@gmail.com>
2025-05-06 18:46:17 -0300

    cartela corrigida

--- programa.py
+++ programa.py
@@ -2,20 +2,20 @@ from funcoes import *
 inteiros = ['1','2','3','4','5']
 cartela = {
     'regra_simples':  {
-        1:0,
-        2:0,
-        3:0,
-        4:0,
-        5:0,
-        6:0
+        1:-1,
+        2:-1,
+        3:-1,
+        4:-1,
+        5:-1,
+        6:-1
     },
     'regra_avancada' : {
-        'sem_combinacao':0,
-        'quadra': 0,
-        'full_house':0,
-        'sequencia_baixa': 0,
-        'sequencia_alta': 0,
-        'cinco_iguais': 0
+        'sem_combinacao':-1,
+        'quadra': -1,
+        'full_house':-1,
+        'sequencia_baixa': -1,
+        'sequencia_alta': -1,
+        'cinco_iguais': -1
     }
 }
 dados = []
